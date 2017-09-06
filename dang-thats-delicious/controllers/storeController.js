c9ffd9cf86afb4ce94688c661345d6dba635554f
David Thorpe <davidthorpe878@gmail.com>
2017-09-06 20:42:13 +0100

    Completed up to part 12

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,12 +1,17 @@
-exports.myMiddleware = (req, res, next) => {
-	req.name = 'David';
-	if(req.name === 'David') {
-		throw Error('That is a stupid name');
-	}
-	next();
-}
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
 	console.log(req.name);
 	res.render('index');
-}
+};
+
+exports.addStore = (req, res) => {
+	res.render('editStore', { title: 'Add Store' });;
+};
+
+exports.createStore = async (req, res) => {
+	const store = await (new Store(req.body)).save();
+	req.flash('success', `Successfully created ${store.name}. Care to leave a review?`)
+	res.redirect(`/store/${store.slug}`);
+};
