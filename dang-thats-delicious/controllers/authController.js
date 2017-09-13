acb18ceb3942a644dcce55bd02541448bdc7a82a
David Thorpe <davidthorpe878@gmail.com>
2017-09-13 21:05:56 +0100

    End of lesson 28
    Emailing resets

--- dang-thats-delicious/controllers/authController.js
+++ dang-thats-delicious/controllers/authController.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const promisify = require('es6-promisify');
+const mail = require('../handlers/mail');
 
 exports.login = passport.authenticate('local', {
 	failureRedirect: '/login',
@@ -39,7 +40,13 @@ exports.forgot = async (req, res) => {
 	await user.save();
 	// 3. Send them an email with the token
 	const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
-	req.flash('success', `A password reset has been emailed to you. ${resetURL}`);
+	await mail.send({
+		user,
+		subject: 'Password reset',
+		resetURL,
+		filename: 'password-reset'
+	})
+	req.flash('success', `A password reset has been emailed to you.`);
 	// 4. Redirect to the login page
 	res.redirect('/login');
 }
