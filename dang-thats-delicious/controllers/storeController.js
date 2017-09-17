c56f9fde778fe52ecffc413dd13c3ec0c83295bb
David Thorpe <davidthorpe878@gmail.com>
2017-09-17 20:22:54 +0100

    app

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -52,9 +52,29 @@ exports.createStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
+	const page = req.params.page || 1;
+	const limit = 4;
+	const skip = (page * limit) - limit;
 	// 1. query database for a list of all stores
-	const stores = await Store.find();
-	res.render('stores', { title: 'Stores', stores });
+	const storesPromise = Store
+		.find()
+		.skip(skip)
+		.limit(limit)
+		.sort({ created: 'desc' });
+		
+	const countPromise = Store.count();
+
+	const [stores, count] = await Promise.all([storesPromise, countPromise]);
+
+	const pages = Math.ceil(count / limit);
+
+	if(!stores.length && skip) {
+		req.flash('info', `hey you asked for page ${page} but that doesn't exist, so I put you on page ${pages}`);
+		res.redirect(`/stores/page/${pages}`);
+		return;
+	}
+
+	res.render('stores', { title: 'Stores', stores, page, pages, count });
 }
 
 const confirmOwner = (store, user) => {
@@ -158,4 +178,9 @@ exports.getHearts = async(req, res) => {
 		_id: { $in: req.user.hearts }
 	});
 	res.render('stores', {title: 'Hearted Stores', stores });
+};
+
+exports.getTopStores = async(req, res) => {
+	const stores = await Store.getTopStores();
+	res.render('topStores', { stores, title: 'Top Stores' });
 };
